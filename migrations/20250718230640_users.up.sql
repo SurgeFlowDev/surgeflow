5e23ff685e06eb2218f334437fcc49bf064a7a4e
Bernardo António Borda d'Água <berna.agua@protonmail.com>
2025-07-19 13:39:02 +0100

    remove old password

--- migrations/20250718230640_users.up.sql
+++ migrations/20250718230640_users.up.sql
@@ -1,7 +1,7 @@
 -- Add up migration script here
 CREATE ROLE app_user
   WITH LOGIN
-       PASSWORD 'YourAppUserPasssword!'
+       PASSWORD ''
        NOSUPERUSER
        NOCREATEDB
        NOCREATEROLE
