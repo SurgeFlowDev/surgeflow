90fab2fff9f4ba83f7cf5592b858a7f99ef27fe9
Bernardo António Borda d'Água <berna.agua@protonmail.com>
2025-08-06 23:35:17 +0100

    features

--- surgeflow/src/workers/mod.rs
+++ surgeflow/src/workers/mod.rs
@@ -1,9 +1,23 @@
+#[cfg(feature = "active_step_worker")]
 pub mod active_step_worker;
+
+#[cfg(feature = "completed_instance_worker")]
 pub mod completed_instance_worker;
+
+#[cfg(feature = "completed_step_worker")]
 pub mod completed_step_worker;
+
+#[cfg(feature = "control_server")]
 pub mod control_server;
+
+#[cfg(feature = "failed_instance_worker")]
 pub mod failed_instance_worker;
+
+#[cfg(feature = "failed_step_worker")]
 pub mod failed_step_worker;
+#[cfg(feature = "new_event_worker")]
 pub mod new_event_worker;
+#[cfg(feature = "new_instance_worker")]
 pub mod new_instance_worker;
+#[cfg(feature = "next_step_worker")]
 pub mod next_step_worker;
