22632e7e62bbf357309d3f1d822b5c9b6569bd5a
Bernardo António Borda d'Água <berna.agua@protonmail.com>
2025-08-07 13:53:50 +0100

    remove unused stuff

--- embedded_adapter/src/lib.rs
+++ embedded_adapter/src/lib.rs
@@ -1,8 +1,4 @@
 use async_channel::{RecvError, SendError};
-use aws_sdk_sqs::{
-    config::http::HttpResponse, error::SdkError, operation::receive_message::ReceiveMessageError,
-    operation::send_message::SendMessageError,
-};
 use surgeflow_types::{FullyQualifiedStep, InstanceEvent, Project, WorkflowInstance};
 
 pub mod dependencies;
@@ -20,20 +16,10 @@ pub enum AwsAdapterError<P: Project> {
     SendInstanceEventError(#[from] SendError<InstanceEvent<P>>),
     #[error("failed to send workflow instance")]
     SendWorkflowInstanceEventError(#[from] SendError<WorkflowInstance>),
-    #[error("no messages received")]
-    NoMessagesReceived,
-    #[error("message without body")]
-    MessageWithoutBody,
-    #[error("message without recepit handle")]
-    MessageWithoutReceptHandle,
     #[error("Failed to deserialize step")]
     DeserializeError(#[source] serde_json::Error),
     #[error("Failed to serialize step")]
     SerializeError(#[source] serde_json::Error),
-    #[error("DynamoDB KV error")]
-    DynamoKvError(#[from] managers::dynamo_kv::DynamoKvError),
     #[error("SQLx error")]
     SqlxError(#[from] sqlx::Error),
 }
-
-// pub type Result<P, T> = std::result::Result<T, AwsAdapterError<P>>;
